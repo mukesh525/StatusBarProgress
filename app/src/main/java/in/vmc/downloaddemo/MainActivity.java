99985c827f1fbf67f21742e37372f46f4406bae7
Mukesh Jha <mukeshjha2007@gmail.com>
2016-03-21 13:38:38 +0530

    1st commit

--- app/src/main/java/in/vmc/downloaddemo/MainActivity.java
+++ app/src/main/java/in/vmc/downloaddemo/MainActivity.java
@@ -2,7 +2,10 @@ package in.vmc.downloaddemo;
 
 
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -11,10 +14,20 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class MainActivity extends AppCompatActivity {
     private NotificationManager mNotifyManager;
     private android.support.v4.app.NotificationCompat.Builder mBuilder;
     int id = 1;
+    private static String file_url = "http://api.androidhive.info/progressdialog/hive.jpg";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
         b1.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View arg0) {
+
                 mNotifyManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
                 mBuilder = new NotificationCompat.Builder(MainActivity.this);
                 mBuilder.setContentTitle("Download")
@@ -33,12 +47,14 @@ public class MainActivity extends AppCompatActivity {
                         .setContentInfo("0%")
                         .setSmallIcon(android.R.drawable.stat_sys_download);
 
-                new Downloader().execute();
+
+                //new Downloader().execute();
+                new Downloader().execute(file_url);
             }
         });
     }
 
-    private class Downloader extends AsyncTask<Void, Integer, Integer> {
+    private class Downloader extends AsyncTask<String, Integer, Integer> {
 
         @Override
         protected void onPreExecute() {
@@ -59,30 +75,76 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
-        protected Integer doInBackground(Void... params) {
+        protected Integer doInBackground(String... params) {
             int i;
-            for (i = 0; i <= 100; i += 5) {
-                // Sets the progress indicator completion percentage
-                publishProgress(Math.min(i, 100));
-                try {
-                    // Sleep for 5 seconds
-                    Thread.sleep(2 * 1000);
-                } catch (InterruptedException e) {
-                    Log.d("TAG", "sleep failure");
+//            for (i = 0; i <= 100; i += 5) {
+//                // Sets the progress indicator completion percentage
+//                publishProgress(Math.min(i, 100));
+//                try {
+//                    // Sleep for 5 seconds
+//                    Thread.sleep(2 * 1000);
+//                } catch (InterruptedException e) {
+//                    Log.d("TAG", "sleep failure");
+//                }
+//            }
+
+            int count;
+            try {
+                URL url = new URL(params[0]);
+                URLConnection conection = url.openConnection();
+                conection.connect();
+                // getting file length
+                int lenghtOfFile = conection.getContentLength();
+
+                // input stream to read file - with 8k buffer
+                InputStream input = new BufferedInputStream(url.openStream(), 8192);
+
+                // Output stream to write file
+                OutputStream output = new FileOutputStream("/sdcard/downloadedfile.jpg");
+
+                byte data[] = new byte[1024];
+
+                long total = 0;
+
+                while ((count = input.read(data)) != -1) {
+                    total += count;
+                    // publishing the progress....
+                    // After this onProgressUpdate will be called
+                    publishProgress((int) ((total * 100) / lenghtOfFile));
+
+                    // writing data to file
+                    output.write(data, 0, count);
                 }
+
+                // flushing output
+                output.flush();
+
+                // closing streams
+                output.close();
+                input.close();
+
+            } catch (Exception e) {
+                Log.e("Error: ", e.getMessage());
             }
+
             return null;
         }
 
         @Override
         protected void onPostExecute(Integer result) {
             super.onPostExecute(result);
+            Intent intent = new Intent();
+            intent.setAction(android.content.Intent.ACTION_VIEW);
+            File file = new File("/sdcard/downloadedfile.jpg"); // set your audio path
+            intent.setDataAndType(Uri.fromFile(file), "image/*");
+            PendingIntent pIntent = PendingIntent.getActivity(MainActivity.this, 0, intent, 0);
             mBuilder.setContentText("Download complete");
             // Removes the progress bar
-
             mBuilder.setOngoing(false);
+            mBuilder.setContentIntent(pIntent);
             mBuilder.setSmallIcon(android.R.drawable.stat_sys_download_done);
             mBuilder.setProgress(0, 0, false);
+
             mNotifyManager.notify(id, mBuilder.build());
         }
     }
